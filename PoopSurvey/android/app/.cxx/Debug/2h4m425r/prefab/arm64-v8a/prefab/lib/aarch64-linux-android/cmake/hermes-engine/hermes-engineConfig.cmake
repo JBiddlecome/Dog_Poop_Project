df95cb6c0d5609095f9e693e0c4b4be7e35e61d3
Jake Biddlecome <jake.biddlecome@gmail.com>
2026-05-13 09:52:18 -0700

    Update Main

---
+++ PoopSurvey/android/app/.cxx/Debug/2h4m425r/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "C:/Users/jakeb/.gradle/caches/8.13/transforms/14d040535af0397c8381d59e9d08b0bc/transformed/hermes-android-0.14.1-debug/prefab/modules/hermesvm/libs/android.arm64-v8a/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/jakeb/.gradle/caches/8.13/transforms/14d040535af0397c8381d59e9d08b0bc/transformed/hermes-android-0.14.1-debug/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
